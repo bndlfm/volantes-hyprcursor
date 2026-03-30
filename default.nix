e69000ac0b97d043d2a22b70fb80ff8afae0014c
bndlfm <firefliesandlightningbugs@gmail.com>
2026-03-30 03:06:48 -0500

    split into light / dark

---
+++ default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "volantes-hyprcursor";
+  version = "0.1";
+  src = fetchFromGitHub {
+    owner = "bndlfm";
+    repo = "volantes-hyprcursor";
+    rev = "504e3325e2e858b58b435731ec68a500776f06f0";
+    sha256 = "sha256-yupOlKvDlJ8zuiPmDpwB9Wyf/hYUHIRNidU6B3QQZn8=";
+  };
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/icons/volantes-light-hyprcursor
+    cp -R ./light/. $out/share/icons/volantes-light-hyprcursor/
+
+    mkdir -p $out/share/icons/volantes-dark-hyprcursor
+    cp -R ./dark/. $out/share/icons/volantes-dark-hyprcursor/
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "volantes cursors in hyprcursor format";
+    downloadPage = "https://github.com/bndlfm/volantes-hyprcursor";
+    homepage = "https://www.deviantart.com/aystic/gallery";
+    license = licenses.gpl2Only;
+    maintainers = with maintainers; [ bndlfm ];
+  };
+})
---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Volantes cursors in hyprcursor format";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system}.default = pkgs.callPackage ./default.nix { };
+    };
+}
